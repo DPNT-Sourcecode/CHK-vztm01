12a62b7380c90296d1fb26917ef3fa578599c9c5
sbx_user1051 <sbx_user1051@169.254.9.117>
2023-10-29 22:39:01 +0000

    Sun Oct 29 22:39:01 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,13 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+
+    int[] prices = {50, 30, 20, 15};
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        String[] units = skus.split(",");
+        int[] count = {0, 0, 0, 0};
+        for(String unit: units)
+            count[unit.charAt(0)-'A']++;
+        return 0;
     }
 }
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,17 @@
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CheckoutSolutionTest {
-  
+
+    CheckoutSolution checkoutSolution = new CheckoutSolution();
+    int[] prices = {50, 30, 20, 15};
+
+    @Test
+    void shouldReturnCostForSingleUnit() {
+        String skus = "A";
+        assert(checkoutSolution.checkout(skus).equals(prices[skus.charAt(0)-'A']));
+    }
 }
