2160629dcceb763437a158afcbca6947d18430f7
sbx_user1051 <sbx_user1051@169.254.9.117>
2023-10-29 22:25:55 +0000

    Sun Oct 29 22:25:55 UTC 2023

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -2,8 +2,7 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        if(friendName.equals("John"))
-            return "Hello," + friendName + "!";
-        return "Hello, World!";
+        return "Hello," + friendName + "!";
     }
 }
+
